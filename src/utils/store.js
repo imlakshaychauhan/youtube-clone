de9bd9162a176f4d54272ec8ca182401e15d725e
Lakshay Chauhan <imlakshaychauhan@gmail.com>
2023-10-24 12:26:40 +0530

    Created slice for caching results

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import appSlice from "./appSlice";
+import searchSlice from "./searchSlice";
 const { configureStore } = require("@reduxjs/toolkit");
 
 const store = configureStore({
     reducer: {
-        app: appSlice
+        app: appSlice,
+        search: searchSlice
     }
 })
 
